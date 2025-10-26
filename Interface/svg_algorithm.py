e6c214f9901d359ed51c519f8e43c3690076881d
Sarakola23 <sarakola@ufl.edu>
2025-10-26 17:09:13 -0400

    made some changes here and there

--- Interface/svg_algorithm.py
+++ Interface/svg_algorithm.py
@@ -13,10 +13,6 @@ import matplotlib.animation as animation
 # parse SVG paths
 from svgpathtools import svg2paths
 
-from svgdigitizer.svg import SVG
-from svgdigitizer.svgplot import SVGPlot
-from svgdigitizer.svgfigure import SVGFigure
-
 # REFERENCE-ISH https://github.com/Bhomik04/image-to-svg/blob/main/python%20practice.py
 
 # function to convert png/jpg to svg
@@ -27,10 +23,12 @@ def conversion_svg(file_path, output_path = "output.svg", potrace_path = "potrac
     width, height = image.size
     # 0 - 255
     img = image.point(lambda x: 0 if x < 128 else 255, '1')
+    # output that potrace expects
+    # https://potrace.sourceforge.net/ 
     img_path = "temp.pbm"
     img.save(img_path)
 
-    # call subprocess since potrace does not work 
+    # call subprocess since potrace does not work for my computer 
     try:
         subprocess.run([potrace_path, img_path, "-s", "-o", output_path], check=True)
         print("SVG created successfully!")
@@ -38,8 +36,11 @@ def conversion_svg(file_path, output_path = "output.svg", potrace_path = "potrac
         if os.path.exists(img_path):    
             os.remove(img_path)
 
+    # list of plack pixels and coordinates we need 
     pixels = np.array(img)
     lines = [(x, y) for y in range(height) for x in range(width) if pixels[y, x] == 0]
+    # return the lines and output_path in which is saved
+    # also width and height for scaling purposes
     return lines, width, height, output_path
 
     # get the size of the image
@@ -64,25 +65,92 @@ def conversion_svg(file_path, output_path = "output.svg", potrace_path = "potrac
     # print("SVG created successfully!")
     # return lines, width, 
 
+
+##############################################################################################
+                    ### NOT NEEDED BUT USED FOR TESTING PURPOSES ###
 # extract the coordinates
-def extract_coordinates(svg_path, samples = 50):
+def extract_coordinates(svg_path, samples=50):
+    # using the svg library to extract paths 
     paths, _ = svg2paths(svg_path)
-    strokes = []
+    all_strokes = []
 
     for path in paths:
+        stroke = []
         for segment in path:
             for t in np.linspace(0, 1, samples):
                 point = segment.point(t)
-                strokes.append((point.real, point.imag))
-    return strokes
+                stroke.append((point.real, point.imag))
+        all_strokes.append(stroke)
+    return all_strokes
+
+
+# Adding a simulation to see how the drawing would look like
+def animation_simulation(strokes):
+    # a4 size in inches to visualize the simulation
+    a4_width_in = 8.27   
+    a4_height_in = 11.69 
+
+    fig, ax = plt.subplots(figsize=(a4_width_in, a4_height_in))
+    ax.set_aspect('equal')
+
+    ax.invert_yaxis()
+
+    all_points = [p for stroke in strokes for p in stroke]
+    # unpack the x, y list from the extract_coordinates function
+    xs, ys = zip(*all_points)
+
+    # add a little margin around the drawing
+    pad_x = (max(xs) - min(xs)) * 0.05
+    pad_y = (max(ys) - min(ys)) * 0.05
+
+    # set the axis limits
+    ax.set_xlim(min(xs) - pad_x, max(xs) + pad_x)
+    ax.set_ylim(min(ys) - pad_y, max(ys) + pad_y)
+    
+    # turn the axis off for better visualization
+    ax.axis('off')
+
+    # get the lines and with update call the lines
+    lines = [ax.plot([], [], lw=2, color='black')[0] for _ in strokes]
+
+    def init():
+        for ln in lines:
+            ln.set_data([], [])
+        return lines
+
+    def update(frame):
+        stroke_idx, point_idx = frame
+        stroke = strokes[stroke_idx]
+        xdata = [p[0] for p in stroke[:point_idx]]
+        ydata = [p[1] for p in stroke[:point_idx]]
+        lines[stroke_idx].set_data(xdata, ydata)
+        return lines
+
+    # Animation frames
+    frames = []
+    for i, stroke in enumerate(strokes):
+        for j in range(1, len(stroke) + 1):
+            frames.append((i, j))
+
+    ani = animation.FuncAnimation(
+        fig, update, frames=frames,
+        init_func=init, blit=True, interval=0.5, repeat=False
+    )
+
+    plt.show() 
+##############################################################################################
 
 # add main
 if __name__ == "__main__":
-    input_path = "drawinggator.jpeg"
+    #input_path = "images/shapes.png"
+    #input_path = "drawinggator.jpeg"
+    #input_path = "images/simple.png"
+    #input_path = "images/roses.jpg"
+    input_path = "images/testing.png"
     output_path = "output.svg"
-    potrace_executable = "potrace"  
+    potrace_executable = "potrace"
 
     conversion_svg(input_path, output_path, potrace_executable)
-    coords = extract_coordinates(output_path, samples=100)
-
-            
+    # animation lines
+    strokes = extract_coordinates(output_path, samples=30)
+    animation_simulation(strokes)
--- README.md
+++ README.md
@@ -1,9 +1,25 @@
 # DrawingGator
 
-Polygraph
-
 #Edited by Jeffrey James
 
 #Add Your Names Below:
-*Sara Kola*
 
+Sara Kola
+
+# Libraries used for the svg_algorithm.py
+1. Pillow: Image Processing
+2. numpy: for numerical arrays
+3. svgwrite: generate the svg file
+4. matplotlib + svgpathtools: for parsing and plotting the lines
+
+# Run the following command:
+```pip install pillow numpy svgwrite matplotlib svgpathtools```
+
+**For the case of potrace since it cannot be installed through the command line:**
+1. Windows:
+
+    Download the following zip and add it to PATH: https://potrace.sourceforge.net/download/1.16/potrace-1.16.win64.zip 
+    Note: Potrace is then called using the subprocess
+
+2. MacOS: ```brew install potrace```
+3. Ubuntu: ```sudo apt install potrace```
